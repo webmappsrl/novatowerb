338b9c6ef4fb275aa1073a1598c6db4caa24a473
gianmarx <gianmarcogagliardi@webmapp.it>
2020-12-18 16:11:24 +0100

    fix update

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -53,7 +53,7 @@ class ArticlePolicy
      */
     public function update(User $user, Article $article)
     {
-        //
+        return true;
     }
 
     /**
@@ -65,7 +65,7 @@ class ArticlePolicy
      */
     public function delete(User $user, Article $article)
     {
-        //
+        return true;
     }
 
     /**
